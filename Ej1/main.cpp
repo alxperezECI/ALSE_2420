975da757e9a1b668dc49016d83b9ff368b151dbf
Miguel Angel Romero <miguel.romero-b@mail.escuelaing.edu.co>
2024-09-05 11:20:00 -0500

    cambio

--- Ej1/main.cpp
+++ Ej1/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
-int main(){
+int main()
+{
 
-  int x, y ,aux;
-  std::cout << "Ingrese dos números enteros: " ;
+  int x, y, aux;
+  std::cout << "Ingrese dos números enteros: ";
   std::cin >> x >> y;
   aux = x;
   x = y;
-  y = aux;
+  std::cout << "nuevo cambio " << std::endl y = aux;
   std::cout << x << ", " << y << std::endl;
-  std::cout << "Hola Alse" << std::endl;
+  std::cout << "Hola Miguel" << std::endl;
   return 0;
 }
