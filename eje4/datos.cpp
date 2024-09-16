61927c41601ac3f9a8070c0ca99c95495f76e74d
Gabriel Gonzalez <gabriel.gonzalez-g@escuelaing.edu.co>
2024-09-16 14:36:22 -0500

    "Parcial 1 Nicolas Ortiz,Gabriel Gonzalez

---
+++ eje4/datos.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main(){
+float x[100];
+float t;
+float inc;
+float fx;
+// Marcar los puntos de cruce por cero        
+if (fx[i] <= 0 && !crossingPoints[i]) 
+{            
+    crossingPoints[i] = true;            
+    crossingPoints[i+1] = true;       
+     }    
+
+for (float i = 0; i < 100; i++)
+{
+    if( inc < 1)
+    {
+        x[i] = i* inc;
+        fx[i] = sin(2 * M_PI * t * X[i]);
+    }
+    cout << "No sirvo"<< endl;
+}
+cin >>"De un valor para t: ">> t >> endl;
+cin >> "De un valor para incremento: " >> inc >> endl;
+cout << "Funcion de f(x): "<<fx[i]<< endl;
+if (crossingPoints[i]) 
+{            
+    std::cout << "*";    
+    }
+return 0;
+
+}
