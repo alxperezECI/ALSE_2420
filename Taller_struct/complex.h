245fe20c0fbb20778c16a9df95e80d477bfe7a36
LuxBlitzz <juan.ocardenas@mail.escuelaing.edu.co>
2024-09-29 23:11:00 -0500

    Entrega taller: Miguel Pardo y Juan Ospina

--- Taller_struct/complex.h
+++ Taller_struct/complex.h
@@ -1,30 +1,111 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
-
+//Miguel pardo y Juan ospina/
 struct complex{
   double re;
   double im;
 };
+// Implemente la siguiente función y explique para qué sirve: Inicializa la variable cuya magnitud es 0 y ángulo 0.
+complex cero(double r =0.0 , double i = 0.0){
+  complex c;
+  c.re = r;
+  c.im = i;
+  return c;
+};
 
-// Implemente la siguiente función y explique para qué sirve
-complex(double r =0. , double i = 0
 
-/* Ajuste la declaración de estas funciones para tener parámetros por referencia seguros
-   e implementelas. Explique qué hace cada una de ellas.
-double angle(complex a);
-double magnitude(complex a);
-complex conjugate(complex a);
+complex conjugate(const complex &a){
+  complex c;
+  c.re =a.re;
+  c.im =-a.im;
+return c;
+}
+/*Double se usa para almacenar valores flotantes de mayor cantidad
+de numeros, ya que el double ocupa hasta 64 bits, esta funcion cambia la conjugacion del numero imaginario y real,
+el parametro "a" hace referencia a un objeto tipo complex, const asegura que el parametro no sera modificado dentro
+de la funcion y el & es para el paso por referencia. Por otro lado, se dice que es sobre carga de operadores porque
+repetidamente se hace uso de la función complex pero con diferente función o acción para operadores existentes en c++ para que ejecuten una acción determinada */
 
-// Ajuste la declaración de estas funciones para tener parámetros por referencia seguros
-// e implementelas. Explique por qué se llama sobrecarga de operadores.
-complex operator+(complex a, complex b);
-complex operator-(complex a, complex b);
-complex operator*(complex a, complex b);
-bool operator==(complex a, complex b); // Implementar utilizando la magnitude y el angle
-bool operator<(complex a, complex b);  // Implementar utilizando la magnitude y el angle
-bool operator>(complex a, complex b);  // Implementar utilizando la magnitude y el angle
+//segundo punto
 
-// Esta sobrecarga es espacial. Indague cómo se debe realizar, explique su correcto funcionamiento
-// e implementela.
+double magnitude(const complex &a){
+  return sqrt(a.re*a.re + a.im*a.im);
+}
+
+/*Double se usa para almacenar valores flotantes de mayor cantidad
+de numeros, ya que el double ocupa hasta 64 bits, esta funcion calcula la magnitud de un numero  imaginario,
+el parametro "a" hace referencia a un objeto tipo complex, const asegura que el parametro no sera modificado dentro
+de la funcion y el & es para el paso por referencia */
+
+double angle(const complex &a){
+  return atan2( a.im, a.re);
+}
+
+/*Double se usa para almacenar valores flotantes de mayor cantidad
+de numeros, ya que el double ocupa hasta 64 bits, esta funcion calcula el angulo un numero imaginario y real
+el parametro "a" hace referencia a un objeto tipo complex, const asegura que el parametro no sera modificado dentro
+de la funcion y el & es para el paso por referencia y el atan2 es el tangente^-1 entre el numero imaginario y el real */
+
+complex operator+(const complex &a, const complex &b){
+  complex c;
+  c.re = a.re + b.re;
+  c.im = a.im+b.im;
+  return c;
+}
+complex operator-(const complex &a, const complex &b){
+  complex c;
+  c.re = a.re - b.re;
+  c.im = a.im - b.im;
+  return c;
+}
+complex operator*(const complex &a, const complex &b){
+  complex c;
+  c.re = a.re * b.re - a.im * b.im;
+  c.im = a.re * b.im + a.im * b.re;
+  return c;
+}
+bool operator==( const complex &a, const complex &b){
+  return magnitude(a) == magnitude(b);
+}
+bool operator>(const complex &a, const complex &b){
+  return magnitude(a) > magnitude(b);
+}
+bool operator<( const complex &a, const complex &b){
+  return angle(a) < angle(b);
+}
 ostream& operator<<(std::ostream& out, complex a);
+/*Al intentar imprimir un número complejo con cout << sum, el compilador no sabe cómo hacerlo porque no se ha sobrecargado
+ el operador << para la estructura complex, por lo tanto se utiliza la sobrecarga espacial para el correcto funcionamiento.
+*/
+int main2(){
+    complex a, b; 
+    complex sum= a + b;
+    complex res = a - b;
+    complex produ = a * b;
+
+
+    std::cout << "Ingrese la parte real de a: ";
+    std::cin >> a.re;
+    std::cout << "Ingrese la parte imaginaria de a: ";
+    std::cin >> a.im;
+    std::cout << "Ingrese la parte real de b: ";
+    std::cin >> b.re;
+    std::cout << "Ingrese la parte imaginaria de b: ";
+    std::cin >> b.im;
+
+    // Imprimir resultados
+    cout << "La magnitud del numero complejo es: " << magnitude(a) << endl;
+    cout << "Ángulo del número complejo: " << angle(a) << " rads" << endl;
+    cout << "Suma: " << sum  << endl;
+    cout << "Resta: " << res << endl;
+    cout << "Multiplicación: " << produ << endl;
+    cout << "¿la magnitud de a es igual a la magnitud de b?: " << (magnitude(a)==magnitude(b) ? "Sí" : "No") << endl;
+    cout << "¿el angulo de a es igual el angulo de b?: " << (angle(a)==angle(b) ? "Sí" : "No") << endl;
+    cout << "¿la magnitud de a es menor que la magnitud de b?: " << (magnitude(a)<magnitude(b) ? "Sí" : "No") << endl;
+    cout << "¿el angulo de a es menor que el angulo de b?: " << (angle(a)<angle(b) ? "Sí" : "No") << endl;
+    cout << "¿la magnitud de a es mayor que la magnitud de b?: " << (magnitude(a)>magnitude(b) ? "Sí" : "No") << endl;
+    cout << "¿el angulo de a es mayor que el angulo de b?: " << (angle(a)>angle(b) ? "Sí" : "No") << endl;
+  return 0;
+};
+
--- Taller_struct/main.cpp
+++ Taller_struct/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "complex.h"
+using namespace std;
 
+// Nombres: Juan David Ospina y Miguel Angel Pardo
 /*  TALLER: 
     - Complete el CMakeLists.txt para configurar adecuadamente el proyecto. 
     - Nombrar el ejecutable usando la variable de configuración PROJECT_NAME
@@ -13,8 +15,6 @@
 	- Se puede desarrollar de forma individual o máximo en parejas.
 */
 
-	
-using namespace std;
 
 int main(){
   complex a, b, c;
@@ -23,27 +23,34 @@ int main(){
   c.re = c.im = 24.59;
 
   complex d =  c + b;
+//a, b y c con numeros complejos pero b, es la conjugacion de a y d es la suma entre c y b
 
-  //d = c - a;
+  //d.im = c.im - a.im;  pues es b es el conjugado de a.
   cout << c << endl;
 
   cout << d << endl;
 
   cout << "Son iguales a y b?: " << ( a==b ) << endl;
   cout << "son iguales b y d?: " << ( b==d ) << endl;
+//se muestra c y d y se verifica si a y b son iguales y si b y d son iguales
 
 // Ahora con arrglos
   complex obj[5];
   obj[0].re = 34.; obj[0].im = -4.;
   obj[1].re = obj[1].im = 45.;
-  obj[2].re 33.33; obj[2].im = -45.;
+  obj[2].re = 33.33; obj[2].im = -45.;
   obj[3].re = 12.; obj[3].im = 25.;
-  obj[4].re = obj[4].im = -56.56 );
+  obj[4].re = obj[4].im = -56.56;
 
   double tmp1, tmp2;
   bool cambio;
+    /* se esta definiendo a tmp1 y a tmp2 como un numero flotante muy grande y el cambio es de tipo booleano indicando que solo puede tener dos 
+  valores, verdadero o falso*/
 
-  // Qué hace este código?
+
+ /*compara los numeros imaginarios con los imaginarios del siguiente numero complejo de igual forma para los reales y los ordena de mayor a menor, si la verificacion
+   de si es menor que el siguiente no se cumple, entonces los guada en tmp2 y tmp1 respectivamente y realizar la ordenacion descendente, odenamiento burbuja*/
+
   do{
     cambio = false;
     for (int i =0; i < 4; ++i ){
