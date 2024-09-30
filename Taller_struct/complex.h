2d03924e99af859d4fa27b8f3413a6317c4f4070
Miguel Angel Romero <miguel.romero-b@mail.escuelaing.edu.co>
2024-09-29 23:59:48 -0500

    ejercicio taller

--- Taller_struct/complex.h
+++ Taller_struct/complex.h
@@ -1,30 +1,107 @@
+/*desarrollado por Miguel Romero y Julian Ballesteros*/
+
 #include <iostream>
+#include <cmath> //para poder usar atan y sqrt
 
 using namespace std;
 
-struct complex{
+struct complex
+{
   double re;
   double im;
 };
 
 // Implemente la siguiente función y explique para qué sirve
-complex(double r =0. , double i = 0
+complex crearcomplex(double r = 0., double i = 0)
+{
+  complex c;
+  complex c.re = r;
+  complex c.im = i;
+
+  return c;
+}
 
 /* Ajuste la declaración de estas funciones para tener parámetros por referencia seguros
-   e implementelas. Explique qué hace cada una de ellas.
-double angle(complex a);
-double magnitude(complex a);
-complex conjugate(complex a);
+   e implementelas. Explique qué hace cada una de ellas.*/
+
+double angle(constant complex &a) // saca el angulo con la formula, queda en radianes.
+{
+  return atan(a.im, a.re);
+}
+double magnitude(constant complex &a) // saca la magnitud con su formula.
+{
+  return sqrt(a.re * a.re + a.im * a.im);
+}
+complex conjugate(constant complex &a) // hace el conjugado.
+{
+  complex res;
+  result.re = a.re;
+  result.im = -a.im;
+  return res;
+}
 
 // Ajuste la declaración de estas funciones para tener parámetros por referencia seguros
 // e implementelas. Explique por qué se llama sobrecarga de operadores.
-complex operator+(complex a, complex b);
-complex operator-(complex a, complex b);
-complex operator*(complex a, complex b);
-bool operator==(complex a, complex b); // Implementar utilizando la magnitude y el angle
-bool operator<(complex a, complex b);  // Implementar utilizando la magnitude y el angle
-bool operator>(complex a, complex b);  // Implementar utilizando la magnitude y el angle
 
+/*se llama sobrecarga de operadores porque de cierto modo se le añade una nueva forma
+de operar, que es con nuestro nuevo tipo de dato, entonces le enseñamos a el operador
+como operar con él, por lo que se "carga" al operador con una nueva "funcion", por ende
+se dice que se sobrecarga */
+
+complex operator+(const complex &a, const complex &b)
+{
+  complex res;
+  res.re = a.re + b.re;
+  res.im = a.im + b.im;
+  return res;
+}
+
+complex operator-(const complex &a, const complex &b)
+{
+  complex res;
+  res.re = a.re - b.re;
+  res.im = a.im - b.im;
+  return res;
+}
+complex operator*(const complex &a, const complex &b)
+{
+  complex res;
+  res.re = a.re * b.re - a.im * b.im;
+  res.im = a.re * b.im + a.im * b.re;
+  return res;
+}
+
+bool operator==(const complex &a, const complex &b)
+{
+  return magnitude(a) == magnitude(b) && angle(a) == angle(b);
+}
+bool operator<(const complex &a, const complex &b)
+{
+  return magnitude(a) < magnitude(b);
+}
+bool operator>(const complex &a, const complex &b)
+{
+  return magnitude(a) > magnitude(b);
+}
 // Esta sobrecarga es espacial. Indague cómo se debe realizar, explique su correcto funcionamiento
 // e implementela.
-ostream& operator<<(std::ostream& out, complex a);
+
+/*entiendo que es una sobrecarga especial porque debe definirse como funcion amiga
+porque no pertenece a un operador normal como la suma, resta, etc. sino que pertenece
+a la libreria estandar, a la cual no tenemos control, por eso la definimos fuera
+como una funcion amiga, de este modo tenemos control sobre como se imprime nuestro obejeto,
+que en este caso es un complex */
+
+ostream &operator<<(std::ostream &out, complex a)
+{
+  out << a.re;
+  if (a.im >= 0)
+  {
+    out << " + " << a.im << "i" << endl;
+  }
+  else
+  {
+    out << " - " << -a.im << "i" << endl;
+  }
+  return out;
+}
--- Taller_struct/main.cpp
+++ Taller_struct/main.cpp
@@ -1,66 +1,81 @@
+/*desarrollado por Miguel Romero y Julian Ballesteros*/
+
 #include <iostream>
 #include "complex.h"
 
-/*  TALLER: 
-    - Complete el CMakeLists.txt para configurar adecuadamente el proyecto. 
+/*  TALLER:
+    - Complete el CMakeLists.txt para configurar adecuadamente el proyecto.
     - Nombrar el ejecutable usando la variable de configuración PROJECT_NAME
     - Usar tanto el main.cpp como el complex.h para construir el ejecutable.
     - Completar el archivo complex.h de acuerdo a las indicaciones internas.
-	- No se debe modificar el código del main.cpp y la aplicación debe 
-	    ejecutarse correctamente. Se deben explicar cada una de las líneas 
-		del main en comentarios.
-	- Enviar el pull request.
-	- Se puede desarrollar de forma individual o máximo en parejas.
+  - No se debe modificar el código del main.cpp y la aplicación debe
+      ejecutarse correctamente. Se deben explicar cada una de las líneas
+    del main en comentarios.
+  - Enviar el pull request.
+  - Se puede desarrollar de forma individual o máximo en parejas.
 */
 
-	
 using namespace std;
 
-int main(){
-  complex a, b, c;
-  a.re = 34.8; a.im = -12.4;
-  b = conjugate(a);
-  c.re = c.im = 24.59;
+int main()
+{
+  complex a, b, c;     // define 3 variables de tipo complex.
+  a.re = 34.8;         // asigna valores a la parte real de a.
+  a.im = -12.4;        // asigna valores a la parte imaginaria de b.
+  b = conjugate(a);    // guarda en b el conjugado de a.
+  c.re = c.im = 24.59; // asigna un valor a la parte imaginaria de c y la asigna a su parte real.
 
-  complex d =  c + b;
+  complex d = c + b; // crea un complex d y le asigna c + d.
 
-  //d = c - a;
-  cout << c << endl;
+  // d = c - a;
+  cout << c << endl; // imprime complex c.
 
-  cout << d << endl;
+  cout << d << endl; // imprime complex d.
 
-  cout << "Son iguales a y b?: " << ( a==b ) << endl;
-  cout << "son iguales b y d?: " << ( b==d ) << endl;
+  cout << "Son iguales a y b?: " << (a == b) << endl; // imprime si a y b son iguales
+  cout << "son iguales b y d?: " << (b == d) << endl; // imprime si b y d son iguales
 
-// Ahora con arrglos
-  complex obj[5];
-  obj[0].re = 34.; obj[0].im = -4.;
-  obj[1].re = obj[1].im = 45.;
-  obj[2].re 33.33; obj[2].im = -45.;
-  obj[3].re = 12.; obj[3].im = 25.;
-  obj[4].re = obj[4].im = -56.56 );
+  // Ahora con arrglos
+  complex obj[5];                 // crea un complejo obj de 5 posiciones
+  obj[0].re = 34.;                // asigna a obj[0] en su parte real un valor
+  obj[0].im = -4.;                // asigna a obj[0] en su parte imaginaria un valor
+  obj[1].re = obj[1].im = 45.;    // asigna a obj[1] un valor igual en su parte real e imaginaria
+  obj[2].re 33.33;                // asigna a obj[2] en su parte real un valor
+  obj[2].im = -45.;               // asigna a obj[2] en su parte imaginaria un valor
+  obj[3].re = 12.;                // asigna a obj[3] en su parte real un valor
+  obj[3].im = 25.;                // asigna a obj[3] en su parte imaginaria un valor
+  obj[4].re = obj[4].im = -56.56; // asigna a obj[4] un valor igual en su parte real e imaginaria
 
-  double tmp1, tmp2;
-  bool cambio;
+  double tmp1, tmp2; // crea dos variables doble
+  bool cambio;       // crea una variable bool
 
   // Qué hace este código?
-  do{
+
+  /*hace el ordenamiento burbuja optimizado para organizar un arreglo de numeros complex
+  y al final con el ciclo for se imprime el obj[]*/
+
+  do
+  {
     cambio = false;
-    for (int i =0; i < 4; ++i ){
-      if( obj[i] < obj[i+1] ){
-      }else{
+    for (int i = 0; i < 4; ++i)
+    {
+      if (obj[i] < obj[i + 1])
+      {
+      }
+      else
+      {
         tmp1 = obj[i].re;
         tmp2 = obj[i].im;
-        obj[i].re = obj[i+1].re ;
-        obj[i].im = obj[i+1].im ;
-        obj[i+1].re = tmp1 ;
-        obj[i+1].im = tmp2 ;
+        obj[i].re = obj[i + 1].re;
+        obj[i].im = obj[i + 1].im;
+        obj[i + 1].re = tmp1;
+        obj[i + 1].im = tmp2;
         cambio = true;
       }
     }
-  }while(cambio == true);
-  
-  for( int i = 0 ; i < 5; ++i )
+  } while (cambio == true);
+
+  for (int i = 0; i < 5; ++i)
     cout << obj[i] << endl;
 
   return 0;
