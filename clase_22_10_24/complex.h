9ac7e86dc11b2f9e728791c820e7356164d8a738
Gabriela Soto <gabriela.soto-g@mail.escuelaing.edu.co>
2024-10-22 10:39:18 -0500

    clase 10 oct

---
+++ clase_22_10_24/complex.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+struct termino{
+  double coef;
+  int exp;
+
+};
+
+class Polinomio{
+  char _variable;
+  int _grado;
+
+  std::list<termino> _terminos;
+
+public:
+  Polinomio(char var);
+  Polinomio(string str);
+  Polinomio operator+(const Polinomio &a);
+  Polinomio operator-(const Polinomio &a);
+
+  bool operator==(const Polinomio &a);
+
+  friend ostream& operator<<(ostream &os, const Polinomio &a);
+
+  bool ordenar();
+  bool simplificar();
+
+  };
