fd38c2922596a07bf0f5281c961350482419bedf
Gabriela Soto <gabriela.soto-g@mail.escuelaing.edu.co>
2024-10-24 09:05:51 -0500

    commit

--- clase_22_10_24/polinomios.cpp
+++ clase_22_10_24/polinomios.cpp
@@ -1,105 +1,127 @@
 #include <iostream>
-#include <cmath>
-#include "complex.h"
+#include <list>
+#include <string>
+#include <sstream>
+#include "polinomio.h"
 
 using namespace std;
 
-void Complex:: Re(const double &r){
-    re = r;
-};
 
-void Complex:: Im(const double &i){
-    im = i;
-};
+Polinomio::Polinomio(char var) {
+    _grado = -1;
+    _variable = var;
+}
 
-double Complex:: Re(){
-    return re;
-};
+Polinomio::Polinomio(string pol) {
+    _grado = -1;
+    _variable = 0;
+    redefinir( pol );
+}
 
-double Complex:: Im(){
-    return im;
-};
+  Polinomio Polinomio::operator+( const Polinomio &a ){
+    Polinomio c('X');
+    return c;
+  }
+  Polinomio Polinomio::operator-( const Polinomio &a ){
+    Polinomio c('X');
+    return c;
+  }
+  bool Polinomio::operator==( const Polinomio &a ){
+    return true;
+  } 
 
-Complex::Complex(double r =0. , double i = 0 ){
-    re = r;
-    im = i;
-};
-
-double Complex::angle()const{
-    return atan2(im, re);
-};
-double Complex::magnitude()const{
-    return sqrt(re*re + im*im);
-};
-Complex Complex::conjugate()const{
-    Complex r;
-    r.re = re;
-    r.im = -im;
-    return r;
-};
-
-Complex Complex::operator+( const Complex &a ){
-    Complex r;
-    r.re = re + a.re;
-    r.im = im + a.im;
-    return r;
-};
-Complex Complex::operator-( const Complex &a ){
-    Complex r;
-    r.re = re - a.re;
-    r.im = im - a.im;
-    return r;
-};
-Complex Complex::operator*( const Complex &a ){
-    Complex r;
-    r.re = re*a.re - im*a.im;
-    r.im = re*a.im + im*a.re;
-    return r;
-};
-bool Complex::operator==( const Complex &a ){
-    bool res;
-    if( magnitude() == a.magnitude() && angle() == a.angle() )
-        res = true;
-    return res;
-}; 
-bool Complex:: operator<( const Complex &a ){
-    bool res;
-    if( magnitude() < a.magnitude() )
-        res = true;
-    else if (magnitude() == a.magnitude())
-    {
-        if (angle() < a.angle())
-        {
-            res = true;
-        }
-    }
-    else
-    {
-        res = false;
-    }
-
-    return res;
-};
-bool Complex::operator>( const Complex &a ){
-    bool res;
-    if( magnitude() > a.magnitude() )
-        res = true;
-    else if (magnitude() == a.magnitude())
-    {
-        if (angle() > a.angle())
-        {
-            res = true;
-        }
-    }
-    else
-    {
-        res = false;
-    }
-    return res;
-}; 
-
-ostream& operator<<(std::ostream& out, Complex a){
-    out << a.re << " + " << a.im << "j";
+ostream& operator<<(std::ostream& out, Polinomio a){
+    out << a.getString();
     return out;
+}
 
-};
+bool Polinomio::ordenar(){
+	return true;
+}
+  
+void Polinomio::nuevoTermino(float c, int p) {
+    termino a(p,c);
+    _terminoL.push_back( a );
+    return;
+}
+  
+bool Polinomio::redefinir(string pol) {
+    borrar();
+    string term;
+    //cout << pol << endl;
+    size_t posM, posm, posT1, posT2, found;
+    posT1 = posT2 = 0;
+    float coeff = 0.;
+    int power = 0;
+
+    while( posT2 != string::npos ){
+        // Encontrando cada termino
+        posM = pol.find("+", posT1 + 1 );
+        posm = pol.find("-", posT1 + 1);
+        // cout << posM << " " << posm << endl;
+        if( posM != string::npos && posm != string::npos )
+            posT2 = min( posM, posm );
+        else if( posM == string::npos )
+            posT2 = posm;
+        else
+            posT2 = posM;
+
+        //cout << posT1 << " " << posT2 << endl;
+        term = pol.substr( posT1, posT2 - posT1 );
+        posT1 = posT2;
+       // cout << term << endl;
+
+        // Porcesando el termino
+        if(_variable == 0){
+            //cout << "variable" << endl;
+            found = term.find_first_not_of("+-1234567890.,");
+            //cout << term[found] << endl;
+            if (found != std::string::npos)
+              _variable = term[found] ;
+        }
+
+        found = term.find( _variable );
+        if( found == string::npos ){
+            //cout << "independiente" << endl;
+            coeff = atof( term.c_str() );
+            power = 0;
+        }else{
+            found = term.find( '^' );
+            if( found != string::npos ){ // Tiene potencia > 1
+                //cout << "pot >1" << endl;
+                power = atoi( term.substr( found+1 ).c_str() );
+                coeff = atof( term.substr( 0, term.size() - found +1 ).c_str() );
+            }else{
+                power = 1;
+                coeff = atof( term.substr( 0, term.size() - 1 ).c_str() );
+            }
+        }
+
+        //cout << "coeff: " << coeff << " y power: " << power << endl;
+        // Adicionar el término encontrado
+        nuevoTermino( coeff, power );
+        _grado = power > _grado ? power : _grado ;
+    }
+    //cout << getString() << endl;
+    return true;
+}
+  
+bool Polinomio::simplificar(){
+	
+	return true;
+}
+
+string  Polinomio::getString(){
+    stringstream stream;
+    std::list<termino>::iterator it;
+    for(it = _terminoL.begin(); it != _terminoL.end(); ++it){
+        if(it->coef > 0) stream << "+";
+            stream << it->coef ;
+        if(it->exp > 0 ){
+            stream << _variable;
+            if( it->exp > 1 )
+                stream << "^" << it->exp;
+        }
+    }
+    return stream.str();
+}
--- clase_22_10_24/polinomios.h
+++ clase_22_10_24/polinomios.h
@@ -6,27 +6,31 @@ using namespace std;
 
 struct termino{
   double coef;
-  int exp;
-
+  int    exp;
+  termino(int e, double c): coef(c), exp(e) {};
 };
+
 
 class Polinomio{
   char _variable;
-  int _grado;
+  int  _grado;
 
-  std::list<termino> _terminos;
+  std::list<termino> _terminoL;
 
 public:
-  Polinomio(char var);
-  Polinomio(string str);
-  Polinomio operator+(const Polinomio &a);
-  Polinomio operator-(const Polinomio &a);
+  Polinomio(char var );
+  Polinomio(string str );
 
-  bool operator==(const Polinomio &a);
+  Polinomio operator+( const Polinomio &a );
+  Polinomio operator-( const Polinomio &a );
+  bool operator==( const Polinomio &a ); 
 
-  friend ostream& operator<<(ostream &os, const Polinomio &a);
+  friend ostream& operator<<(std::ostream& out, Polinomio a);
 
+  bool borrar();
   bool ordenar();
   bool simplificar();
-
-  };
+  string getString();
+  void nuevoTermino(float c, int p) ;
+  bool redefinir(string pol);
+};
