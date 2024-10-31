d36d0a1674941c45cbe02ecf675d5f3417e8c19f
JoseV64 <jose.velasco-m@mail.escuelaing.edu.co>
2024-10-31 11:32:10 -0500

    Update empleado.cpp

--- p2_pra/empleado.cpp
+++ p2_pra/empleado.cpp
@@ -9,6 +9,42 @@ Empleado::Empleado(const std::string& nombre, const std::string& apellido,
     _salarioMensual(salarioMensual) 
     {
         _horasExDia.clear();
+
+       
     }
 
+void Empleado::agregarHorasExtras(int dia, float horas){
+
+    HorasExtras aux (dia, horas);
+
+    _horasExDia.push_back(aux);
+
+};
+
+double Empleado::val_horasextra(){
+    const double val_hora = _salarioMensual / 192;
+    double tHorasExtras = 0;
+    std::vector<HorasExtras>::const_iterator it;
+
+    for ( it = _horasExDia.begin(); it != _horasExDia.end(); ++it) {
+        tHorasExtras += it->second;
+    }
+
+    return tHorasExtras * val_hora;
+}
+
+bool Empleado::min(){
+    std::vector<HorasExtras>::const_iterator it;
+    double aux = 0;
+
+
+    double tHorasExtras = 0;
+    for ( it = _horasExDia.begin(); it != _horasExDia.end(); ++it) {
+        tHorasExtras += it->second;
+    }
+
+
+    
+
+};
 
